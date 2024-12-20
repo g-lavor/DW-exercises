c6cc234c51da0d05265c42df8d49ed11f946ada0
Gabriel Lavôr <103610943+g-lavor@users.noreply.github.com>
2024-12-20 19:23:01 -0300

    Add files via upload

---
+++ IP API/js/main.js
@@ -0,0 +1,27 @@
+document.getElementById("btnGetIp").addEventListener("click", function () {
+    const inputIp = document.getElementById("inputIpAddress").value.trim();
+    document.getElementById("alert-info").innerHTML = "";
+
+    fetch(`https://ipinfo.io/${inputIp}/json?token=token`)
+        .then(response => response.json())
+        .then(data => {
+            const { ip, city = "-", country = "-", org = "-", region = "-", loc = "-" } = data;
+            
+            const newRow = `
+                <tr>
+                    <td>${ip}</td>
+                    <td>${org}</td>
+                    <td>${country}</td>
+                    <td>${city}</td>
+                    <td>${region}</td>
+                    <td>${loc}</td>
+                    <td><button class="btn btn-danger btn-sm" onclick="removeRow(this)">Remover</button></td>
+                </tr>
+            `;
+            document.getElementById("ipTableBody").insertAdjacentHTML("beforeend", newRow);
+        });
+});
+
+function removeRow(button) {
+    button.closest("tr").remove();
+}
